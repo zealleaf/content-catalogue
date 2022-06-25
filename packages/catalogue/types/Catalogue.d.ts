5b3d3312b006ab2b1de28ac1b4ce59905cc8ee8d
leaf <106396432+zealleaf@users.noreply.github.com>
2022-06-25 16:00:47 +0800

    Delete packages/catalogue/types directory

--- packages/catalogue/types/Catalogue.d.ts
+++
@@ -1,8 +0,0 @@
-interface propsData {
-  contentMark: string
-  scrollBehavior: 'smooth' | 'none'
-  diyWrap: string
-  diyItem: string
-}
-declare const Catalogue: React.FC<propsData>
-export default Catalogue
--- packages/catalogue/types/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as LeafveinCatalogue } from './Catalogue'
