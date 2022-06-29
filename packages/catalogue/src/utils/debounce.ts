61c5944fcabe7c724a7d80a3f77bb2f993d9f444
zealleaf <feb.zealleaf@gmail.com>
2022-06-29 23:01:21 +0800

    fix: debounce ts

--- packages/catalogue/src/utils/debounce.ts
+++ packages/catalogue/src/utils/debounce.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const debounce = (fn: (p: any) => any, delay: number) => {
-  let timer: number | NodeJS.Timeout // 维护一个 timer
+  let timer: NodeJS.Timeout // 维护一个 timer
   return function (...args: unknown[]) {
     const a = args
     if (timer) {
